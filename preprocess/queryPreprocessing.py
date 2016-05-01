710c42d5026e3ef19bb7f978475b60070382a284
RenqinCai <jetcai1900@gmail.com>
2016-05-01 15:22:22 -0400

    1. add functions to preprocess query 2. transform APP reviews and descriptions

--- preprocess/queryPreprocessing.py
+++ preprocess/queryPreprocessing.py
@@ -14,41 +14,46 @@ def loadFile(fileName, queryList, queryMap):
 		relevantAPP = line[1]
 		relevance = line[2]
 
-	if query not in queryList:
-		queryList.append(query)
+		if query not in queryList:
+			queryList.append(query)
 
-	if query not in queryMap.keys():
-		queryMap.setdefault(query, rawLine)
+		if query not in queryMap.keys():
+			queryMap.setdefault(query, [])
+			queryMap[query].append(rawLine)
+		else:
+			queryMap[query].append(rawLine)
 
 def writeQueryIDFile(fileName, queryList, queryMap):
-	f = open(fileName)
+	f = open(fileName, "w")
 
 	for i in range(len(queryList)):
 		query = queryList[i]
-		f.write(i+"\t"+query+"\n") 
+		f.write(str(i)+"\t"+query+"\n") 
 
 	f.close()
 
 def writeQuerySourceFile(fileName, queryList, queryMap):
-	f = open(fileName)
+	f = open(fileName, "w")
 
 	for i in range(len(queryList)):
 		query = queryList[i]
 
-		queryLine = queryMap[query]
-		f.write(i+"\t"+queryLine+"\n")
+		queryLineList = queryMap[query]
+
+		for queryLine in queryLineList:
+			f.write(str(i)+"\t"+queryLine)
 
 	f.close()
 
 queryMap = {}
 queryList = []
 
-inputFileName = "./query-app-relevance"
+inputFileName = "../../../Data/TextMiningProject/appsearch_data/query-app-relevance"
 
-loadFile(fileName, queryList, queryMap)
+loadFile(inputFileName, queryList, queryMap)
 
 queryIDFileName = "./queryID.txt"
 querySourceFileName = "./query-app-relevance.txt"
 
-writeQueryIDFile(fileName, queryList, queryMap)
+writeQueryIDFile(queryIDFileName, queryList, queryMap)
 writeQuerySourceFile(querySourceFileName, queryList, queryMap)
